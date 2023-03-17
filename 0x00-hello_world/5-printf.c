b439648ac46e6a7b8910a8027c16d5dbd2db6c9a
BcodezMUJJE <billbrightmatege@gmail.com>
2023-03-17 02:37:22 -0700

    task printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the prints
+ *
+ * Return: 0 when you succeed
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
