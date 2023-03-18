cfd809ef5ffb6cd3aa4f5fa0caadd70a67dc09f3
BcodezMUJJE <billbrightmatege@gmail.com>
2023-03-17 22:51:03 -0700

    lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * maim -main function
+ * main - alphabet in lower and upper case
  *
  * Return: always 0
  */
@@ -10,8 +10,9 @@ int main(void)
 char i;
 
 for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
 for (i = 'A' ; i <= 'Z' ; i++)
-putchar(i);
+	putchar(i);
 putchar('\n');
 return (0);
 }
