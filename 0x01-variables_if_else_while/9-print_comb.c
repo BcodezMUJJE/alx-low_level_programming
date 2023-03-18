11b6ab7ed20a68561164b787815c6642bb76c770
BcodezMUJJE <billbrightmatege@gmail.com>
2023-03-18 03:19:11 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,13 @@ int i;
 
 for (i = 0 ; i < 10 ; i++)
 {
-putchar(i);
+if (i == 9)
+	putchar(i + '0');
+else
+{putchar(i + '0');
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
+}
 return (0);
 }
