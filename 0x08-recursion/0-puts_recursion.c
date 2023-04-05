83fb6b2da2e757d2a9cc087895ca90fe3ab81768
BcodezMUJJE <billbrightmatege@gmail.com>
2023-04-05 03:16:49 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
+
 #include "main.h"
-
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: input character
- * Return: void
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(s + 1);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
-_putchar('\n');
-}
+
