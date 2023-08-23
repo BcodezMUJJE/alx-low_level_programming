57937dd161644511c4a3dd8236e57ba7fb7cde47
BcodezMUJJE <billbrightmatege@gmail.com>
2023-08-23 03:24:29 -0700

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""The function that defines the perimeter of an island in a grid"""
+
+def island_perimeter(grid):
+    """
+    This returns the perimeter of the island described in grid.
+
+    Args:
+    grid (list[list[int]]): A list of lists representing the grid.
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if row == rows - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == cols - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
